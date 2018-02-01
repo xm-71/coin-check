2e489993e5eb9205b7fe0ee278b10448bb8043c9
Alex Cannon <alexcannon2@live.com>
2018-02-01 17:35:49 -0500

    read me page updated

--- JS/angularroute.js
+++ JS/angularroute.js
@@ -2,7 +2,7 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : 'pages/home.html'
+        templateUrl : 'pages/currencycheck.html'
     })
     .when("/price", {
         templateUrl : 'pages/currencycheck.html'
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # coin-check
+
+Description
+=======
+A clean app that implements user registration, authentication, and routing.
+
+Frameworks
+======
+Angular 1.6
+Angular Route
+Bootstrap 4
+Firebase
+Font Awesome
+cryptocoins icon pack
+CoinMarketCap API
+
+Installation
+======
+
+Download and play
